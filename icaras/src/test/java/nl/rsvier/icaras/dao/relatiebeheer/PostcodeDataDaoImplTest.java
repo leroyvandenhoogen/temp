60770a0ac2299b0abf85978c0175f381f79dfac7
Chi <chijoung@gmail.com>
2015-07-14 13:51:12 +0200

    DaoTest Adres en PostcodeData wordt correct opgehaald

--- icaras/src/test/java/nl/rsvier/icaras/dao/relatiebeheer/PostcodeDataDaoImplTest.java
+++ icaras/src/test/java/nl/rsvier/icaras/dao/relatiebeheer/PostcodeDataDaoImplTest.java
@@ -1,8 +1,11 @@
 package nl.rsvier.icaras.dao.relatiebeheer;
 
+import static org.junit.Assert.*;
+
 import javax.transaction.Transactional;
 
 import nl.rsvier.icaras.core.relatiebeheer.Adres;
+import nl.rsvier.icaras.core.relatiebeheer.PostcodeData;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -22,10 +25,32 @@ public class PostcodeDataDaoImplTest {
 	@Test
 	@Transactional
 	public void testZoekAdres() {
-		String postcode = "3972TP";
+		String postcode = "7940XX";
 		Adres adres = postcodeDao.zoekAdres(postcode);
 		
-		System.out.println(adres);
+		assertNotNull(adres);
+		assertTrue(adres.getStraat().equals("Troelstraplein"));
+		assertTrue(adres.getPostcode().equals(postcode));
+		assertTrue(adres.getPlaats().equals("Meppel"));
+		assertTrue(adres.getProvincie().equals("Drenthe"));
 	}
 	
+	@Test
+	@Transactional
+	public void testZoekPostcodeData() {
+		String postcode = "7940XX";
+		
+		PostcodeData pData = postcodeDao.zoekPostcodeData(postcode);
+		
+		assertNotNull(pData);
+		assertTrue(pData.getStraat().equals("Troelstraplein"));
+		assertTrue(pData.getPostcode().equals(postcode));
+		assertTrue(pData.getStad().equals("Meppel"));
+		assertTrue(pData.getProvincie().equals("Drenthe"));
+		assertTrue(pData.getGemeente().equals("Meppel"));
+		assertTrue(pData.getLon().equals(6.1977201775604));
+		assertTrue(pData.getLat().equals(52.7047653217626));
+
+		
+	}
 }
