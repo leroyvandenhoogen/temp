1e6ecb4eeb38be9d65446063f4bc40165f683772
Chi <chijoung@gmail.com>
2015-07-21 15:00:31 +0200

    Mailservice werkt op ghetto manier

--- icaras/src/test/java/nl/rsvier/icaras/RandomPassTester.java
+++ icaras/src/test/java/nl/rsvier/icaras/RandomPassTester.java
@@ -1,9 +1,13 @@
 package nl.rsvier.icaras;
 
+import nl.rsvier.icaras.util.MailMail;
 import nl.rsvier.icaras.util.PasswordGenerator;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.transaction.TransactionConfiguration;
@@ -17,4 +21,20 @@ public class RandomPassTester {
 	public void generatePass() {
 		System.out.println(PasswordGenerator.generate());
 	}
+	
+	@Test
+	public void mailTest() {
+		{
+	    	ApplicationContext context = 
+	             new ClassPathXmlApplicationContext("Spring-Mail.xml");
+	 
+	    	MailMail mm = (MailMail) context.getBean("mailMail");
+	        mm.sendMail("icaras@rsvier.nl",
+	    		   "@gmail.com",
+	    		   "Testing123", 
+	    		   "Testing only \n\n Hello Spring Email Sender");
+	 
+	    }
+	}
+
 }
